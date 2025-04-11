ace1cd45378ec1e63091fdecfb99dd96edae3cac
lednew10 <wendel.f10santos@gmail.com>
2025-04-11 15:33:44 -0300

    commit do tio - menu e subtração

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -2,31 +2,55 @@ import java.util.Scanner;
 
 public class CalculadoraSimples {
 
-        public static void main(String[] args) {
-            //Declaração de variáveis
-            int n1, n2, total;
+    public static void main(String[] args) {
+        int n1 = 0, n2 = 0, total = 0, op;
 
-            //Criação e instancia do objeto de entrada
-            Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("\n\t\t\t -- Calculadora Simples --\n");
 
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Sair");
 
-            //Apresentação
-            System.out.println("\n\t\t\t -- Calculadora Simples --\n");
+        System.out.print("Opção: ");
+        op = entrada.nextInt();
 
-            //Entradas
+        if(op == 1){
             System.out.println("Soma Simples: ");
             System.out.print("Informe N1: ");
             n1 = entrada.nextInt();
-            System.out.print("Informe N2:");
+            System.out.print("Informe N2: ");
             n2 = entrada.nextInt();
 
-
-            //Processamento
             total = n1 + n2;
 
-
-            //Saída
             System.out.println(n1 + " + " + n2 + " = " + total);
+        } 
+        else if(op == 2){
+            System.out.println("Subtração Simples:");
+            System.out.print("Informe N1: ");
+            n1 = entrada.nextInt();
+            System.out.print("Informe N2: ");
+            n2 = entrada.nextInt();
+
+            total = n1 - n2;
+
+            System.out.println(n1 + " - " + n2 + " = " + total);
+        } 
+        else if(op == 3){
+            System.out.println("Encerrando a calculadora... Forte abraço!");
+        } 
+        else {
+            System.out.println("Opção inválida.");
         }
+
+        entrada.close();
+    }
 }
+
+
+
+
+                              
+
